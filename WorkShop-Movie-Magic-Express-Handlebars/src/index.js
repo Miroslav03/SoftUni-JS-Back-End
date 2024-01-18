7128e6fb1264f8560bc4f83267642dd86ae3f2c6
Miroslav Ivanov <znaka5@abv.bg>
2024-01-19 00:30:13 +0200

    Render home page

--- WorkShop-Movie-Magic-Express-Handlebars/src/index.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/index.js
@@ -11,6 +11,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+app.use(express.static('src/public'))
+
 app.get('/', (req, res) => {
     res.render('home', { layout: false });
 })
