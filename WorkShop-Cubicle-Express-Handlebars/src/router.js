4f4e9d3e209e6ca206aaba9f3d6a1b1b7c5900c2
Miroslav Ivanov <znaka5@abv.bg>
2024-01-19 18:41:12 +0200

    Add cube controller and create page

--- WorkShop-Cubicle-Express-Handlebars/src/router.js
+++ WorkShop-Cubicle-Express-Handlebars/src/router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
+
 
 router.use(homeController);
-
+router.use(cubeController);
 
 router.get('*', (req, res) => {
     res.render('404')
