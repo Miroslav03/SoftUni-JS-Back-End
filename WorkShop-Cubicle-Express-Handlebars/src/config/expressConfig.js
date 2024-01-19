46a25e073a4de0440a2b015386cadb35246c54a9
Miroslav Ivanov <znaka5@abv.bg>
2024-01-19 18:53:35 +0200

    Added parser

--- WorkShop-Cubicle-Express-Handlebars/src/config/expressConfig.js
+++ WorkShop-Cubicle-Express-Handlebars/src/config/expressConfig.js
@@ -3,7 +3,7 @@ const express = require('express');
 function configExpress(app) {
 
     app.use(express.static('src/public'))
-
+    app.use(express.urlencoded({ extended: false }))
 
     return app;
 }
