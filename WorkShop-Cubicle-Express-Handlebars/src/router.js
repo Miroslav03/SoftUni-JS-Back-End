4259d5119d4b40db86b39bd55ab7c8f8462fd3c8
Miroslav Ivanov <znaka5@abv.bg>
2024-01-19 18:38:32 +0200

    Add 404

--- WorkShop-Cubicle-Express-Handlebars/src/router.js
+++ WorkShop-Cubicle-Express-Handlebars/src/router.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 
-router.use(homeController)
+router.use(homeController);
 
+
+router.get('*', (req, res) => {
+    res.render('404')
+});
 
 module.exports = router;
