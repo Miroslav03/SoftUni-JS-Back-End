ce8e4e0c33b2342115ebb28e219411219e2a639a
Miroslav Ivanov <znaka5@abv.bg>
2024-01-19 15:15:12 +0200

    add body parses

--- WorkShop-Movie-Magic-Express-Handlebars/src/config/expressConfig.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/config/expressConfig.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 function configExpress(app) {
     app.use(express.static('src/public'));
-    
+    app.use(express.urlencoded({ extended: false }));
+
     return app;
 }
 
