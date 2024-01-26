da30e3c1e5941678a8d7331f9f8209aaed4904df
Miroslav Ivanov <znaka5@abv.bg>
2024-01-26 17:33:21 +0200

    Implementing create page with db

--- WorkShop-Movie-Magic-Express-Handlebars/src/services/movieService.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/services/movieService.js
@@ -1,27 +1,8 @@
-const movies = [{
-    _id: 1,
-    title: 'Harry Potter and the Deathly Hallows Part 1',
-    genre: 'Action',
-    director: 'David Yates',
-    year: '2011',
-    imageUrl: '/img/Harry Potter.jpg',
-    rating: '5',
-    description: 'As Harry, Ron and Hermione race against time and evil to destroy the Horcruxes, they uncover the existence of the three most powerful objects in the wizarding world: the Deathly Hallows.'
-}, {
-    _id: 2,
-    title: 'Shrek',
-    genre: 'Comedy',
-    director: 'Andrew Adamson',
-    year: '2001',
-    imageUrl: '/img/Shrek.jpg',
-    rating: '5',
-    description: 'Shrek is an anti-social and highly territorial ogre who loves the solitude of his swamp, and enjoys fending off mobs and intruders. His life is interrupted after the dwarfish Lord Farquaad of Duloc exiles a vast number of fairy-tale creatures, who inadvertently end up in the swamp.'
-}];
+const Movie = require('../models/Movie');
 
-exports.create = (movieData) => {
-    movieData._id = movies.length + 1;
-    movies.push(movieData);
-};
+
+exports.create = async (movieData) => Movie.create(movieData);
+
 
 exports.getAll = () => {
     return movies.slice();//Deep copy to cut the reference to original arr
