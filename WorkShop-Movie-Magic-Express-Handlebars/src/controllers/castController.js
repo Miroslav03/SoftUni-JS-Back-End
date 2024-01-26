e10b4899ee8293c60be7fa28d2acb7aae9603281
Miroslav Ivanov <znaka5@abv.bg>
2024-01-26 18:43:25 +0200

    Create cast page
    Delete unnecessary photos

--- WorkShop-Movie-Magic-Express-Handlebars/src/controllers/castController.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/controllers/castController.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 
+const castService = require('../services/castService');
+
+
 router.get('/create', (req, res) => {
     res.render('./cast/create');
 })
 
+router.post('/create', async (req, res) => {
+    const castData = req.body;
+    try {
+        await castService.create(castData);
+        res.redirect('/');
+    } catch (error) {
+        console.log(error.message);
+        res.redirect('create')
+    }
+})
 
 module.exports = router;
