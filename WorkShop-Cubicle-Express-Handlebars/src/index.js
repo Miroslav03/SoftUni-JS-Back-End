54f6efb2d439e47a0c7975b79fa3154b3fed4b10
Miroslav Ivanov <znaka5@abv.bg>
2024-01-28 19:52:34 +0200

    Set up mongoose

--- WorkShop-Cubicle-Express-Handlebars/src/index.js
+++ WorkShop-Cubicle-Express-Handlebars/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
@@ -10,7 +11,10 @@ const configHandlebars = require('./config/handlebars')(app);
 
 app.use(router);
 
+mongoose.connect(`mongodb://localhost:27017/cubicle`)
+    .then(() => {
+        console.log('DB Connected!');
+        app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}`); })
+    })
+    .catch(err => console.error('Cannot connect to DB!'))
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-})
