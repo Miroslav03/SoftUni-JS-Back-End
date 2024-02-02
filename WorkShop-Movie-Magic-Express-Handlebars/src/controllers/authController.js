211b232390213f7852f446bafcf9284f72625535
Miroslav Ivanov <znaka5@abv.bg>
2024-02-02 16:11:01 +0200

    Added register page
    Hashing password in User model and adding bcrypt

--- WorkShop-Movie-Magic-Express-Handlebars/src/controllers/authController.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/controllers/authController.js
@@ -1,8 +1,20 @@
-const router  = require('express').Router();
+const router = require('express').Router();
+
+const authService = require('../services/authService');
 
 
-router.get('/register',(req,res) =>{
+router.get('/register', (req, res) => {
+    res.render('./auth/register');
+});
 
+router.post('/register', (req, res) => {
+    const userData = req.body;
+    authService.regitser(userData);
+    res.redirect('/auth/login');
+});
+
+router.get('/login', (req, res) => {
+    res.render('./auth/login');
 });
 
 
--- WorkShop-Movie-Magic-Express-Handlebars/src/models/User.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/models/User.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     },
 })
 
-const User = mongoose.model('User',userSchema);
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
