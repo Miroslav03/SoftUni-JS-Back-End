ffda8c54ec2563860f937cbeb277a20ee80195a1
Miroslav Ivanov <znaka5@abv.bg>
2024-02-02 17:00:00 +0200

    Add cookieParser

--- WorkShop-Movie-Magic-Express-Handlebars/src/config/expressConfig.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/config/expressConfig.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 function configExpress(app) {
     app.use(express.static('src/public'));
     app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser())
 
     return app;
 }
