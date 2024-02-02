958e8a7efc9fa5bbf9a381fdd02abd6a913ade80
Miroslav Ivanov <znaka5@abv.bg>
2024-02-02 17:24:25 +0200

    Add editMovie page

--- WorkShop-Movie-Magic-Express-Handlebars/src/controllers/movieController.js
+++ WorkShop-Movie-Magic-Express-Handlebars/src/controllers/movieController.js
@@ -46,8 +46,12 @@ router.post('/movies/:movieId/attachCast', async (req, res) => {
     res.redirect(`/movies/${movieId}/attachCast`);
 })
 
-router.get('/movies/:movieId/edit', (req, res) => {
-    res.render('./movie/edit')
+router.get('/movies/:movieId/edit', async (req, res) => {
+    const movieId = req.params.movieId;
+
+    const movie = await movieService.getOne(movieId).lean();
+
+    res.render('./movie/edit', { movie });
 });
 
 module.exports = router;
