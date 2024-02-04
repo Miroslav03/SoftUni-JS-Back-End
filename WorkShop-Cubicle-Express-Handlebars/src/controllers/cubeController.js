792e70bf7ec22c80bec5a25f6b57b30ba6c0e68f
Miroslav Ivanov <znaka5@abv.bg>
2024-02-04 21:24:51 +0200

    Add delete page and fix edit

--- WorkShop-Cubicle-Express-Handlebars/src/controllers/cubeController.js
+++ WorkShop-Cubicle-Express-Handlebars/src/controllers/cubeController.js
@@ -24,15 +24,30 @@ router.get('/details/:id', async (req, res) => {
 router.get('/edit/:id', async (req, res) => {
     const cubeId = req.params.id;
     const cubeData = await cubeService.getOne(cubeId).lean();
-    res.render('editCube', { cubeData });
+    const difficultyArr = cubeService.difficultyLevelOptions(cubeData.difficulty)
+
+    res.render('editCube', { cubeData, difficultyArr });
 });
 
 router.post('/edit/:id', async (req, res) => {
     const cubeId = req.params.id;
     const cubeData = req.body;
+    console.log(cubeData)
     await cubeService.edit(cubeId, cubeData);
     res.redirect(`/details/${cubeId}`);
 });
 
+router.get('/delete/:id', async (req, res) => {
+    const cubeId = req.params.id;
+    const cubeData = await cubeService.getOne(cubeId).lean();
+    const difficultyArr = cubeService.difficultyLevelOptions(cubeData.difficulty)
+    res.render('deleteCube', { cubeData, difficultyArr });
+});
+
+router.post('/delete/:id', async (req, res) => {
+    const cubeId = req.params.id;
+    await cubeService.delete(cubeId);
+    res.redirect(`/`);
+});
 
 module.exports = router;
--- WorkShop-Cubicle-Express-Handlebars/src/services/cubeService.js
+++ WorkShop-Cubicle-Express-Handlebars/src/services/cubeService.js
@@ -29,3 +29,27 @@ exports.attach = async (cubeId, accId) => {
 }
 
 exports.edit = async (cubeId, cubeData) => Cube.findByIdAndUpdate(cubeId, cubeData);
+
+exports.difficultyLevelOptions = (cubeDifficulty) => {
+    const data = {
+        1: 'Very Easy',
+        2: 'Easy',
+        3: 'Medium (Standard 3x3)',
+        4: 'Intermediate',
+        5: 'Expert',
+        6: 'Hardcore',
+    }
+    const difficultyArr = [];
+
+    difficultyArr.push({ value: Number(cubeDifficulty), name: `${cubeDifficulty} - ${data[cubeDifficulty]}` });
+
+    for (const [key, value] of Object.entries(data)) {
+        if (key !== cubeDifficulty) {
+            difficultyArr.push({ value: Number(key), name: `${key} - ${value}` });
+        }
+    }
+
+    return difficultyArr
+};
+
+exports.delete = (cubeId) => Cube.findByIdAndDelete(cubeId)
