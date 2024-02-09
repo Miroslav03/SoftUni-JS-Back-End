ffb1e43b48d8ed6b55767f98536d48f1c5f4a324
Miroslav Ivanov <znaka5@abv.bg>
2024-02-09 18:27:00 +0200

    Update User model validation

--- WorkShop-Cubicle-Express-Handlebars/src/models/User.js
+++ WorkShop-Cubicle-Express-Handlebars/src/models/User.js
@@ -5,14 +5,15 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique:true,
+        unique: true,
+        minLength: [5, 'Username must be at least 5 charecters!']
     },
 
     password: {
         type: String,
         required: true,
-        match:[/[a-zA-Z0-9]/,'Password must be only English letters and digits!'],
-        minLength:[5,'Password must be at least 5 symbols!']
+        match: [/[a-zA-Z0-9]/, 'Password must be only English letters and digits!'],
+        minLength: [5, 'Password must be at least 5 symbols!']
     },
 })
 
